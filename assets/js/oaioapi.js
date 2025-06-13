24c58a13a8b8b66dcd03bce4f8fb06c28f2f1bd6
alxfed <alxfed@gmail.com>
2025-06-12 19:59:00 -0500

    initial commit

--- assets/js/oaioapi.js
+++ assets/js/oaioapi.js
@@ -89,7 +89,7 @@ self.onmessage = async function (event) {
 		};
 
 		console.log('Worker: Making API call to OpenAI API with payload:', finalApiPayload);
-		// const apiCallResponse = await fetch(machineConfig.apiUrl, apiOptions);
+		const apiCallResponse = await fetch(machineConfig.apiUrl, apiOptions);
 
 		if (!apiCallResponse.ok) {
 			let errorDetails = await apiCallResponse.text();

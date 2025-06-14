a29b94c14ed5f7129bb223bc0e7b46933800544e
alxfed <alxfed@gmail.com>
2025-06-13 19:54:43 -0500

    initial commit

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -12,4 +12,29 @@
       }
     }));
   };
+
+  // Override localStorage.removeItem
+  const originalRemoveItem = localStorage.removeItem;
+  localStorage.removeItem = function (key) {
+    originalRemoveItem.apply(this, arguments);
+    window.dispatchEvent(new CustomEvent('localStorageChanged', {
+      detail: {
+        type: 'removeItem',
+        key,
+        timestamp: Date.now()
+      }
+    }));
+  };
+
+  // Override localStorage.clear
+  const originalClear = localStorage.clear;
+  localStorage.clear = function () {
+    originalClear.apply(this);
+    window.dispatchEvent(new CustomEvent('localStorageChanged', {
+      detail: {
+        type: 'clear',
+        timestamp: Date.now()
+      }
+    }));
+  };
 })();

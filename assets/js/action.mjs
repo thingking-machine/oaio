3bdfad6f23d8f1b46da5dc1152a37192252f9d71
alxfed <alxfed@gmail.com>
2025-08-01 06:39:42 -0500

    initial commit

--- assets/js/action.mjs
+++ assets/js/action.mjs
@@ -67,6 +67,8 @@ class MachineApp {
    */
   _applyQueryParameters() {
     const queryParams = new URLSearchParams(window.location.search);
+    // set default instruction
+    this.settings.llm['instructions_file'] = this.settings.machine['instructions_file']
     for (const [key, value] of queryParams.entries()) {
       if (['temperature'].includes(key)) {
         const numValue = parseFloat(value);
